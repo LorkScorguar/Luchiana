c2f37e9c79771c200af145a2a2f675ecd2707880
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-04-28 15:24:56 +0200

    remove api key and ask user

--- server/installServer.py
+++ server/installServer.py
@@ -18,13 +18,16 @@ smtpPort="587"
 smtpPortSSL="993"
 
 #Freebase
-freebase_apikey="AIzaSyA-WMfgx3SUL4RAWRKiStbU8JV2al0DPQ0"
+freebase_apikey=FREEBASE_KEY
 service_url="https://www.googleapis.com/freebase/v1/search"
 service_url2="https://www.googleapis.com/freebase/v1/topic"
 defaultFreebaseResponse="Impossible de trouver l'information"
 
 #database
 listeFichiers=["actions-fr","informatique-fr","politesse-fr","simple-fr"]
+
+#Weather
+weather_apikey=OPENWEATHER_KEY
 """
 
 import getpass
@@ -37,9 +40,13 @@ ip=input("Quelle est votre adresse ip?")
 clef=input("Entrer la clef qui sera utiliser pour chiffrer les informations critiques: ")
 email=input("Entrer le compte gmail réservé pour votre IA, ou le votre: ")
 password=getpass.getpass("Entrer le mot de passe associé: ")
+freebase=input("Entrer votre clef api pour freebase:")
+weather=input("Entrer votre clef api pour openweathermap:")
 config=config.replace("IPADDRESS","'"+ip+"'")
 config=config.replace("CLEF","'"+clef+"'")
 config=config.replace("EMAIL","'"+email+"'")
+config=config.replace("FREEBASE_KEY","'"+freebase+"'")
+config=config.replace("OPENWEATHER_KEY","'"+weather+"'")
 config=config.replace("PASSWORD","'"+Securite.vigenere(password,clef,"1")+"'")
 fichier.write(config)
 fichier.close()
