6f9ea6ed645c78460fd927b260fb9f05b19bee25
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 12:38:50 +0200

    pylint compliance

--- server/Securite.py
+++ server/Securite.py
@@ -2,11 +2,12 @@
 module contenant les fonction de securite
 chiffrement, dechiffrement, decryptage etc
 """
-import time
-import re
+DICO = []
+global DICO
 
-def vigenere(phrase,clef,operation):
-    #1=chiffre 2=dechiffre
+
+def vigenere(phrase, clef, operation):
+    """ utiliser pour chiffrer/dechiffrer les communications client/serveur 1=chiffre 2=dechiffre"""
     sortie, i = "", 0
     for caract in phrase:   #parcours de la chaine a traiter
         if operation == "1":    #chiffrement
@@ -21,48 +22,47 @@ def vigenere(phrase,clef,operation):
                 i = 0
     return sortie
 
-"""clef=input("entrer la clef:")
-texte=input("entrer le texte:")
-c=vigenere(texte,clef,"1")
-print(c)
-d=vigenere(c,clef,"2")
-print(d)"""
+#clef=input("entrer la clef:")
+#texte=input("entrer le texte:")
+#c=vigenere(texte,clef,"1")
+#print(c)
+#d=vigenere(c,clef,"2")
+#print(d)
 
 
 def cherche_anagramme(mot):
-    global dico
-    res=[]
+    """fonction de recherche d'anagramme d'un mot"""
+    global DICO
+    res = []
     lettres = dict((l, mot.count(l)) for l in set(mot))
     if len(mot) == 1:
         res.append(mot)
     else:
-        for ana in dico:
-            if len(ana)==len(mot):
-                anag=True
+        for ana in DICO:
+            if len(ana) == len(mot):
+                anag = True
                 for lettre in mot:
                     if lettre not in ana:
-                        anag=False
-                if anag==True:
+                        anag = False
+                if anag:
                     lettres2 = dict((l, ana.count(l)) for l in set(ana))
-                    if lettres==lettres2:
+                    if lettres == lettres2:
                         res.append(ana)
-
     return res
 
 def init():
-    global dico
-    dico=[]
-    dic=open("dico/dico-fr.txt","r")
+    """Charge le dictionnaire en ram"""
+    global DICO
+    dic = open("DICO/DICO-fr.txt", "r")
     for line in dic:
-        dico.append(line.strip())
+        DICO.append(line.strip())
 
-"""start=time.time()
-print("init en cours")
-init()
-print("init en "+str(time.time()-start)+"sec")
-while 1:
-    mot=input("Entrer le mot à anagrammer:")
-    start=time.time()
-    a=cherche_anagramme(mot)
-    print("Résultat en :"+str(a)+"\n"+str(time.time()-start)+"sec")
-"""
+# start=time.time()
+# print("init en cours")
+# init()
+# print("init en "+str(time.time()-start)+"sec")
+# while 1:
+#     mot=input("Entrer le mot à anagrammer:")
+#     start=time.time()
+#     a=cherche_anagramme(mot)
+#     print("Résultat en :"+str(a)+"\n"+str(time.time()-start)+"sec")
