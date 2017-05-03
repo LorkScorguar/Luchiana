4cec7767de34888a9b7cb619ee546cf35506a38c
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 09:36:39 +0200

    be compliant with pylint

--- client3/Securite.py
+++ client3/Securite.py
@@ -2,11 +2,9 @@
 module contenant les fonction de securite
 chiffrement, dechiffrement, decryptage etc
 """
-import time
-import re
 
-def vigenere(phrase,clef,operation):
-    #1=chiffre 2=dechiffre
+def vigenere(phrase, clef, operation):
+    """ utiliser pour chiffrer/dechiffrer les communications client/serveur 1=chiffre 2=dechiffre"""
     sortie, i = "", 0
     for caract in phrase:   #parcours de la chaine a traiter
         if operation == "1":    #chiffrement
@@ -21,10 +19,9 @@ def vigenere(phrase,clef,operation):
                 i = 0
     return sortie
 
-"""clef=input("entrer la clef:")
-texte=input("entrer le texte:")
-c=vigenere(texte,clef,"1")
-print(c)
-d=vigenere(c,clef,"2")
-print(d)
-"""
+#clef=input("entrer la clef:")
+#texte=input("entrer le texte:")
+#c=vigenere(texte,clef,"1")
+#print(c)
+#d=vigenere(c,clef,"2")
+#print(d)
