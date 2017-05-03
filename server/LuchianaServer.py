a12a8563c4613126a01c1e75ab606bd89c314784
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 14:53:33 +0200

    pylint compliance

--- server/LuchianaServer.py
+++ server/LuchianaServer.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3.3
+"""
+Main module
+"""
+
+import threading
+import time
+import builtins
+
 import Cerveau
 import Securite
 import System
 import Reload
 import Server
-import threading
-import time
-import builtins
-import Mail
-import time
 import Proxy
 import Config
 
 if __name__ == '__main__':
     Proxy.connectProxy("http")
-    starttime=time.time()
+    STARTTIME = time.time()
     print("Initialisation en cours")
     HOST, PORT = Config.address, Config.port
     LS = Server.ThreadedTCPServer((HOST, PORT), Server.ThreadedTCPRequestHandler)
     Securite.init()
     Cerveau.init()
-    builtins.init=0
-    server_thread = threading.Thread(target=LS.serve_forever)
-    server_thread.start()
-    print("initialisation réalisé en "+str(time.time()-starttime)+"sec")
+    builtins.init = 0
+    SERVER_THREAD = threading.Thread(target=LS.serve_forever)
+    SERVER_THREAD.start()
+    print("initialisation réalisé en "+str(time.time()-STARTTIME)+"sec")
     print('Serving')
-#    a=System.MyMonitoringThread('Monitoring')
-#    a.start()
-    b=threading.Thread(None,Reload.verify,None)
-    b.start()
-
-
+#    MON = System.MyMonitoringThread('Monitoring')
+#    MON.start()
+    REL = threading.Thread(None, Reload.verify, None)
+    REL.start()
