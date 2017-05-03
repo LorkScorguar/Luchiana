48a3bc30cc8bdf0552f10327cf9ef0d87831fc16
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 17:15:26 +0200

    pylint compliance

--- server/Cerveau.py
+++ server/Cerveau.py
@@ -1,3 +1,7 @@
+"""
+Module de reflexion
+Permet de trouver la réponse la plus appropriée pour chaque question
+"""
 #Modules de base
 import re
 import datetime
@@ -13,169 +17,161 @@ import Mail
 import Config
 import FileManager
 
-global infos
-global action
-global code
-global reponse
-global listeFichiers
-reponse=Config.defaultResponse
-listeFichiers=Config.listeFichiers
-infos=[]
-code=0
-action=""
+reponse = Config.defaultResponse
+listeFichiers = Config.listeFichiers
+INFOS = []
+CODE = 0
+ACTION = ""
 
 
-Articles=["le","la","les","l","un","une","des","du"]
-PronomsSujet=["j","je","tu","il","elle","on","nous","vous","ils","elles"]
-PronomsPossessif=["mon","ton","son","ma","ta","sa","mes","tes","ses"]
-Question=["comment","quel","quelle","quand","combien","qui","que","qu'est-ce","qu'est ce"]
-Ponctuation=[".","!","?","...",",",";",":","(",")"]
-
-def init():
-    global dico
-    dico=[]
-    dic=open("dico/dico-fr.txt","r")
-    for line in dic:
-        dico.append(line.strip())
+Articles = ["le", "la", "les", "l", "un", "une", "des", "du"]
+PronomsSujet = ["j", "je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles"]
+PronomsPossessif = ["mon", "ton", "son", "ma", "ta", "sa", "mes", "tes", "ses"]
+Question = ["comment", "quel", "quelle", "quand", "combien", "qui", "que",\
+            "qu'est-ce", "qu'est ce"]
+Ponctuation = [".", "!", "?", "...", ",", ";", ":", "(", ")"]
 
 
-def analyse(phrase,username):
-    global action
-    global code
-    global infos
+def analyse(phrase, username):
+    """fonction d'analyse de l'entrée utilisateur"""
+    global ACTION
+    global CODE
+    global INFOS
     global reponse
-    type=""
-    resIn,resOut="",""
-    act,typeAct,rep="","",""
-    if re.search("[A-Z].*",phrase):
-        m=re.search("[A-Z].*",phrase)
-        resIn=m.group(0)
-    if action=="":#gestion passage initial
-        if re.search("(calcul|combien|pythagore|thales|perimetre|surface|volume|triangle){1,}[-+*\/]?\d+",phrase,re.IGNORECASE) or re.search("([-+*\/]+\d+)+",phrase):
-        #if re.search("toto",phrase):
-            if re.search("[triangle,rectangle]",phrase,re.IGNORECASE):
-                phrase+=" pythagore"
-            type="T"
-            rep=Math.run(phrase)
+    typ = ""
+    resIn, resOut = "", ""
+    act, typeAct, rep = "", "", ""
+    if re.search("[A-Z].*", phrase):
+        regex = re.search("[A-Z].*", phrase)
+        resIn = regex.group(0)
+    if ACTION == "":#gestion passage initial
+        if re.search(r"(calcul|combien|pythagore|thales|perimetre|surface|"\
+                     r"volume|triangle){1, }[-+*\/]?\d+", phrase,\
+                     re.IGNORECASE) or re.search(r"([-+*\/]+\d+)+", phrase):
+            if re.search("[triangle, rectangle]", phrase, re.IGNORECASE):
+                phrase += " pythagore"
+            typ = "T"
+            rep = Math.run(phrase)
         else:
-            possible=0
-            for file in listeFichiers:
-                fichier=open("database/"+file,"r")
+            possible = 0
+            for fich in listeFichiers:
+                fichier = open("database/"+fich, "r")
                 for ligne in fichier:
-                    temp=ligne.split(";")
-                    mots=temp[0]
-                    i=0
+                    temp = ligne.split(";")
+                    mots = temp[0]
+                    i = 0
                     while i < len(mots):
-                        if re.search(mots,phrase,re.IGNORECASE):
-                            t=mots.split(".*")
-                            if len(t)>possible:
-                                possible=len(t)
-                                typeAct=temp[1]
-                                act=temp[2]
-                                reponse=temp[3]
-                        i+=2
-            type="T"
-            listRep=reponse.split("/")
-            rep=choice(listRep)
+                        if re.search(mots, phrase, re.IGNORECASE):
+                            t = mots.split(".*")
+                            if len(t) > possible:
+                                possible = len(t)
+                                typeAct = temp[1]
+                                act = temp[2]
+                                reponse = temp[3]
+                        i += 2
+            typ = "T"
+            listRep = reponse.split("/")
+            rep = choice(listRep)
 
             #remplacement du %IN% dans l'action
-            listeAct=act.split(" ")
+            listeAct = act.split(" ")
             for i in range(len(listeAct)):
-                if listeAct[i].strip()=="%IN%":
-                    #listeAct[i]="'"+resIn.strip()+"'"
-                    listeAct[i]="'"+phrase.strip()+"'"
-            act=" ".join(listeAct)
-            if typeAct=="bash":#récupération des infos en plus
-                if re.search("%OUT%",reponse):
-                    resOut=subprocess.check_output(act,shell=True)
+                if listeAct[i].strip() == "%IN%":
+                    listeAct[i] = "'"+phrase.strip()+"'"
+            act = " ".join(listeAct)
+            if typeAct == "bash":#récupération des infos en plus
+                if re.search("%OUT%", reponse):
+                    resOut = subprocess.check_output(act, shell=True)
                 else:
-                    resOut=System.command(act)
-            elif typeAct=="python":
-                resOut,infos=eval(act)
-                action=infos[0]
-                code=infos[1]
-
-            listeMot=rep.split(" ")
+                    resOut = System.command(act)
+            elif typeAct == "python":
+                resOut, INFOS = eval(act)
+                ACTION = INFOS[0]
+                CODE = INFOS[1]
+            listeMot = rep.split(" ")
             #remplacement des %IN% et %OUT% dans la réponse
-            if typeAct!="python" or (typeAct=="python" and code==0):#si reponse directe
+            #si reponse directe
+            if typeAct != "python" or (typeAct == "python" and CODE == 0):
                 for i in range(len(listeMot)):
-                    if listeMot[i].strip()=="%OUT%":
+                    if listeMot[i].strip() == "%OUT%":
                         try:
-                            resOut=resOut.decode()
+                            resOut = resOut.decode()
                         except:
-                            resOut=resOut
-                        listeMot[i]=resOut.strip()
+                            resOut = resOut
+                        listeMot[i] = resOut.strip()
                 for i in range(len(listeMot)):
-                    if listeMot[i].strip()=="%IN%":
-                        listeMot[i]=resIn.strip()
-                rep=" ".join(listeMot)
-            else:# si besoin de precision, on affiche le resultat de la fonction
+                    if listeMot[i].strip() == "%IN%":
+                        listeMot[i] = resIn.strip()
+                rep = " ".join(listeMot)
+            else:# si besoin de precision,  on affiche le resultat de la fonction
                 try:
-                    rep=resOut.decode()
+                    rep = resOut.decode()
                 except:
-                    rep=resOut
-            if action=="fichier" and code==0:
-                type="F"
+                    rep = resOut
+            if ACTION == "fichier" and CODE == 0:
+                typ = "F"
             else:
-                type="T"
-        if code==0:
-            action,infos="",""
-    elif action!="":
-        type="T"
-        if re.search("annule",phrase,re.IGNORECASE):
-            infos=[]
-            code=0
-            action=""
+                typ = "T"
+        if CODE == 0:
+            ACTION, INFOS = "", ""
+    elif ACTION != "":
+        typ = "T"
+        if re.search("annule", phrase, re.IGNORECASE):
+            INFOS = []
+            CODE = 0
+            ACTION = ""
         else:
-            fct=infos[len(infos)-1]
-            resOut,infos=eval(fct+"("+str(code)+",'"+str(phrase)+"',"+str(infos)+")")
-            code=infos[1]
-            if code==0:
-                action=""
-                infos=[]
-                listRep=reponse.split("/")
-                rep=choice(listRep)
-                listeMot=rep.split(" ")
+            fct = INFOS[len(INFOS)-1]
+            resOut, INFOS = eval(fct+"("+str(CODE)+", '"+str(phrase)+\
+                                 "', "+str(INFOS)+")")
+            CODE = INFOS[1]
+            if CODE == 0:
+                ACTION = ""
+                INFOS = []
+                listRep = reponse.split("/")
+                rep = choice(listRep)
+                listeMot = rep.split(" ")
                 for i in range(len(listeMot)):
-                    if listeMot[i].strip()=="%OUT%":
+                    if listeMot[i].strip() == "%OUT%":
                         try:
-                            resOut=resOut.decode()
+                            resOut = resOut.decode()
                         except:
-                            resOut=resOut
-                        listeMot[i]=resOut.strip()
+                            resOut = resOut
+                        listeMot[i] = resOut.strip()
                 for i in range(len(listeMot)):
-                    if listeMot[i].strip()=="%IN%":
-                        listeMot[i]=resIn.strip()
-                rep=" ".join(listeMot)
+                    if listeMot[i].strip() == "%IN%":
+                        listeMot[i] = resIn.strip()
+                rep = " ".join(listeMot)
             else:
-                action=""
-                code=0
-                infos=[]
+                ACTION = ""
+                CODE = 0
+                INFOS = []
                 try:
-                    resOut=resOut.decode()
+                    resOut = resOut.decode()
                 except:
-                    resOut=resOut
-                rep=resOut
+                    resOut = resOut
+                rep = resOut
     else:
-        type="T"
-        rep=reponse
-    if re.search("\d{4}-\d{2}-\d{2}",rep): #reformattage des dates
-        m=re.search("\d{4}-\d{2}-\d{2}",rep)
-        date=m.group(0)
-        d=date.split("-")
-        newDate=d[2]+" "+d[1]+ " "+d[0]
-        rep=re.sub("\d{4}-\d{2}-\d{2}",newDate,rep)
-        rep=re.sub("T\d+:\d+","",rep)#suppression de l'heure
-    reponse=Config.defaultResponse
-    return type,str(rep.strip())
+        typ = "T"
+        rep = reponse
+    if re.search(r"\d{4}-\d{2}-\d{2}", rep): #reformattage des dates
+        regex = re.search(r"\d{4}-\d{2}-\d{2}", rep)
+        date = regex.group(0)
+        dat = date.split("-")
+        newDate = dat[2]+" "+dat[1]+ " "+dat[0]
+        rep = re.sub(r"\d{4}-\d{2}-\d{2}", newDate, rep)
+        rep = re.sub(r"T\d+:\d+", "", rep)#suppression de l'heure
+    reponse = Config.defaultResponse
+    return typ, str(rep.strip())
 
 
 def run():
+    """Fonction de test"""
     while 1:
-        quest=input(">")
-        if quest =="quit":
+        quest = input(">")
+        if quest == "quit":
             exit(0)
-        t,rep=analyse(quest,"florent")
+        _, rep = analyse(quest, "florent")
         print("==>"+rep)
 
 #Proxy.connectProxy("https")
