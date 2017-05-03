30d6832960c50a4874f2061cd0b3fbeb9931ada4
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 10:41:10 +0200

    pylint compliance

--- client3/LuchianaClient.py
+++ client3/LuchianaClient.py
@@ -1,115 +1,121 @@
 #!/usr/bin/python3.3
-#http://www.pygtk.org/docs/pygtk/gtk-stock-items.html
+"""Module Principal"""
 import getpass
 import re
-import time
 import threading
-import subprocess
-import os
 
 import Client
 #import Audio
 #import GoogleSpeechToText as GSTT
 import FileManager
 
-stop=False
-son=False
+SON = False
 
 
 def connection():
+    """Gére la connection au serveur"""
     Client.connect()
-    identify=0
-    essai=0
-    while identify==0 and essai < 3:
-        pseudo=input("Entrer votre pseudonyme: ")
-        password=getpass.getpass("Entrer le mot de passe: ")
-        Client.sendMsg("L",pseudo+";,;"+password)
-        rep=Client.receiveMsg()
-        if(rep[1][9:]=="1"):
-            identify=1
+    identify = 0
+    essai = 0
+    while identify == 0 and essai < 3:
+        pseudo = input("Entrer votre pseudonyme: ")
+        password = getpass.getpass("Entrer le mot de passe: ")
+        Client.sendMsg("L", pseudo+";,;"+password)
+        rep = Client.receiveMsg()
+        if rep[1][9:] == "1":
+            identify = 1
             break
         else:
-            if(essai<2):
+            if essai < 2:
                 print("Vous avez fais une erreur\nVeuillez recommencer")
-            if(essai==2):
+            if essai == 2:
                 print("Trop d'erreurs, au revoir")
                 quit()
-            essai=essai+1
+            essai = essai+1
     return identify
 
 
 class ListenPort(threading.Thread):
-    def __init__(self, nom = ''):
+    """Classe pour l'écoute du port et retour à l'utilisateur"""
+    def __init__(self, nom=''):
+        """init"""
         threading.Thread.__init__(self)
         self.nom = nom
-        self._stopevent = threading.Event( )
+        self._stopevent = threading.Event()
     def run(self):
-        global son
+        """main"""
+        global SON
         while not self._stopevent.isSet():
-            typ,received = Client.receiveMsg()
-            if typ=="N":
-                r=Client.notify(received)
-                if r=="ko":
+            typ, received = Client.receiveMsg()
+            if typ == "N":
+                rec = Client.notify(received)
+                if rec == "ko":
                     print("\n-->%s" % received)
-            elif typ=="F":
-                r=received.split(";")
-                FileManager.convertFileReceive(r[0],r[1])
-                r=Client.notify("notif;Transfert;Fichier "+r[1]+" bien reçu;4")
-                if r=="ko":
-                    print("\n-->Fichier "+r[1]+" bien reçu")
+            elif typ == "F":
+                rec = received.split(";")
+                FileManager.convertFileReceive(rec[0], rec[1])
+                rec = Client.notify("notif;Transfert;Fichier "+rec[1]+" bien reçu;4")
+                if rec == "ko":
+                    print("\n-->Fichier "+rec[1]+" bien reçu")
             else:
                 print("\n-->%s" % received)
-                if son:
-                    adire=received[:-14]
+                #if SON:
+                    #adire = received[:-14]
                     #Audio.parle(adire)
     def stop(self):
-        self._stopevent.set( )
+        """stop le thread"""
+        self._stopevent.set()
 
 class ListenUser(threading.Thread):
-    def __init__(self, nom = ''):
+    """Classe pour l'écoute de l'utilisateur et envoie au serveur"""
+    def __init__(self, nom=''):
+        """init"""
         threading.Thread.__init__(self)
         self.nom = nom
-        self._stopevent = threading.Event( )
+        self._stopevent = threading.Event()
     def run(self):
-        global son
-        global a
+        """Main"""
+        global SON
+        global TLP
         while not self._stopevent.isSet():
             data = input(">")
             if data == "quit":
-                a.stop()
+                TLP.stop()
                 self.stop()
             elif data == "Tais toi!":
-                son=False
+                SON = False
             elif data == "Parle!":
-                son=True
-            elif re.search("je.*envoie.*fichier",data):
-                m=re.search("[A-Za-z]*.\.[A-Za-z]*",data)
-                fileName=m.group(0)
-                data2,infos=FileManager.sendFichier(0,fileName)
-                if infos[1]==0:
-                    Client.sendFile(data2,fileName)
+                SON = True
+            elif re.search("je.*envoie.*fichier", data):
+                regex = re.search(r"[A-Za-z]*.\.[A-Za-z]*", data)
+                fileName = regex.group(0)
+                data2, infos = FileManager.sendFichier(0, fileName)
+                if infos[1] == 0:
+                    Client.sendFile(data2, fileName)
                 else:
-                    choix=input(data2)
-                    data2,infos=FileManager.sendFichier(1,choix,infos)
+                    choix = input(data2)
+                    data2, infos = FileManager.sendFichier(1, choix, infos)
             elif len(data):
-                Client.sendMsg("T",str(data))
+                Client.sendMsg("T", str(data))
             else:
                 continue
     def stop(self):
-        self._stopevent.set( )
+        """Ferme la connection"""
+        self._stopevent.set()
         Client.clientQuit()
 
 
-i=connection()
+CONNECT = connection()
 #import Proxy
 #Proxy.connectProxy('https')
-if i == 1:
-    r=Client.notify("notif;Connection;Vous etes desormais connecte a Luchiana;1")
-    if r=="ko":
+if CONNECT == 1:
+    RES = Client.notify("notif;Connection;Vous etes desormais"
+                        "connecte a Luchiana;1")
+    if RES == "ko":
         print("\n-->Vous etes desormais connecte a Luchiana")
-    a=ListenPort('lp')
-    b=ListenUser('lu')
-    #c=threading.Thread(None, GSTT.listen_for_speech, None)
-    a.start()
-    b.start()
-#    c.start()
+    TLP = ListenPort('lp')
+    TLU = ListenUser('lu')
+    #tlfs=threading.Thread(None, GSTT.listen_for_speech, None)
+    TLP.start()
+    TLU.start()
+#   tlfs.start()
