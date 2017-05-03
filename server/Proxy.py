09636e3e8e20974bbd06fce1314d491e12ebfabf
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 14:50:23 +0200

    pylint compliance

--- server/Proxy.py
+++ server/Proxy.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3.3
-
+"""
+Gestion d'un proxy
+"""
 import urllib.request
-import ssl
 
 
 def connectProxy(type):
+    """Configure un proxy"""
     proxy_info = {
-    'host' : "lx5804.res.jcdecaux.org",
-    'port' : 8000 # or 8080 or whatever
+        'host' : "lx5804.res.jcdecaux.org",
+        'port' : 8000 # or 8080 or whatever
     }
 
     # build a new opener that uses a proxy requiring authorization
-    proxy_support = urllib.request.ProxyHandler({type : "http://%(host)s:%(port)d" % proxy_info})
-    opener = urllib.request.build_opener(proxy_support, urllib.request.HTTPHandler)
+    proxy_support = urllib.request.ProxyHandler({type:"http://%(host)s"\
+                                                      ":%(port)d" % proxy_info})
+    opener = urllib.request.build_opener(proxy_support,\
+                                         urllib.request.HTTPHandler)
 
     # install it
     urllib.request.install_opener(opener)
 
     # use it
-    """req=urllib.request.Request(url="https://www.google.fr")
-    page=urllib.request.urlopen(req).read().decode('utf-8','ignore')
-    print(page)"""
-    
+    #req=urllib.request.Request(url="https://www.google.fr")
+    #page=urllib.request.urlopen(req).read().decode('utf-8','ignore')
+    #print(page)
+
 #connectProxy("https")
