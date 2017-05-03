3a5671ce537bad5c9963faa069739ab7a639777b
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 15:09:23 +0200

    pylint compliance

--- server/GPIO.py
+++ server/GPIO.py
@@ -4,18 +4,22 @@ module pour la gestion des ports GPIO en Python
 
 import subprocess
 
-def active(port,mode):
-    subprocess.check_output("gpio mode "+str(port)+" "+mode,shell=True)
+def active(port, mode):
+    """active le port"""
+    subprocess.check_output("gpio mode "+str(port)+" "+mode, shell=True)
     return 0
 
 def allume(port):
-    subprocess.check_output("gpio write "+str(port)+" 1",shell=True)
+    """allume le port"""
+    subprocess.check_output("gpio write "+str(port)+" 1", shell=True)
     return 0
 
 def eteint(port):
-    subprocess.check_output("gpio write "+str(port)+" 0",shell=True)
+    """eteint le port"""
+    subprocess.check_output("gpio write "+str(port)+" 0", shell=True)
     return 0
 
 def getValue(port):
-    subprocess.check_output("gpio read "+str(port),shell=True)
+    """Récupére la valeur du port"""
+    subprocess.check_output("gpio read "+str(port), shell=True)
     return 0
