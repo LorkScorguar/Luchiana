1853b65d27b1aa6df942f7626f4679804a5ff3c3
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 15:28:32 +0200

    pylint compliance

--- server/Server.py
+++ server/Server.py
@@ -1,4 +1,6 @@
-import socket
+"""
+Module de gestion de la communication avec le client
+"""
 import threading
 import socketserver
 import datetime
@@ -12,89 +14,93 @@ import Cerveau
 import Config
 import FileManager
 
-clef=Config.clef
+CLEF = Config.clef
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-    def sendMsg(self,typ,message):
-        t=str(message)
-        d=Securite.vigenere(t,clef,"1")
-        self.request.sendall(bytes(typ+";"+d+"\n",'utf-8'))
+    """Classe de gestion de la communication"""
+    def sendMsg(self, typ, message):
+        """Fonction d'envoie de message"""
+        text = str(message)
+        dec = Securite.vigenere(text, CLEF, "1")
+        self.request.sendall(bytes(typ+";"+dec+"\n", 'utf-8'))
 
-    def sendFile(self,data,fileName):
-        self.request.sendall(bytes("F;","utf-8")+data+bytes(";"+fileName,"utf-8"))
+    def sendFile(self, data, fileName):
+        """Fonction d'envoie de fichier"""
+        self.request.sendall(bytes("F;", "utf-8")+data+bytes(";"+fileName, "utf-8"))
 
     def handle(self):
-        logFile=open("logs","a")
-        builtins.sendHandler=self
-        builtins.init=1
-        essai=0
-        identify=0
+        """Fonction principale"""
+        logFile = open("logs", "a")
+        builtins.sendHandler = self
+        builtins.init = 1
+        essai = 0
+        identify = 0
         while essai < 3:
             rec = self.request.recv(1024).strip()
-            if len(rec)>0:
-                clair = Securite.vigenere(rec[2:].decode(),clef,"2")
-                r = clair.split(";,;")
-                password=r[1]
-                username=r[0]
-                mp=Database.searchUser(username).strip()
-                if mp=="invalide":
-                    self.sendMsg("L","identify=0")
-                    essai=essai+1
+            if len(rec) > 0:
+                res = Securite.vigenere(rec[2:].decode(), CLEF, "2").split(";,;")
+                password = res[1]
+                username = res[0]
+                mp = Database.searchUser(username).strip()
+                if mp == "invalide":
+                    self.sendMsg("L", "identify=0")
+                    essai = essai+1
                 else:
-                    if hashlib.sha224(password.encode('utf-8')).hexdigest()==mp:
-                        identify=1
-                        self.sendMsg("L","identify=1")
+                    if hashlib.sha224(password.encode('utf-8')).hexdigest() == mp:
+                        identify = 1
+                        self.sendMsg("L", "identify=1")
                         logFile.write(username+";"+datetime.datetime.now().isoformat()+"\n")
                         break
                     else:
-                        self.sendMsg("L","identify=0")
-                        essai=essai+1
+                        self.sendMsg("L", "identify=0")
+                        essai = essai+1
             else:
-                builtins.init=0
+                builtins.init = 0
                 print("client deconnecte")
                 break
 
         while 1 and identify == 1:
-            suivi=open("suivi","r")
-            nbUser=0
+            suivi = open("suivi", "r")
+            nbUser = 0
             for ligne in suivi:
-                nbUser=int(ligne.strip())
-            nbUser=nbUser+1
-            suivi=open("suivi","w")
+                nbUser = int(ligne.strip())
+            nbUser = nbUser+1
+            suivi = open("suivi", "w")
             suivi.write(str(nbUser))
             suivi.close()
-            d = self.request.recv(1024).strip()
-            if len(d)>0:
-                t=d.decode()
-                temp=t.split(";")
-                typ=temp[0]
+            dat = self.request.recv(1024).strip()
+            if len(dat) > 0:
+                text = dat.decode()
+                temp = text.split(";")
+                typ = temp[0]
                 if typ == "T":
-                    data = Securite.vigenere(temp[1],clef,"2")
+                    data = Securite.vigenere(temp[1], CLEF, "2")
                     cur_thread = threading.current_thread()
-                    typ,answer=Cerveau.analyse(data,username)
+                    typ, answer = Cerveau.analyse(data, username)
 
-                    if username =="lork":
-                        answer+=" (from"+str(cur_thread.name)+")"
-                    self.sendMsg(typ,answer)
+                    if username == "lork":
+                        answer += " (from"+str(cur_thread.name)+")"
+                    self.sendMsg(typ, answer)
                 elif typ == "F":
-                    complete=False
+                    complete = False
                     while not complete:
-                        d += self.request.recv(1024).strip()
-                        t=d.decode()
-                        if re.search(";[A-Za-z]*.\.[A-Za-z]*",t):
-                            complete=True
-                    temp=t.split(";")
-                    data=temp[1]
-                    fileName=temp[2]
-                    FileManager.convertFileReceive(data,fileName)
-                    self.sendMsg("T","Fichier "+fileName+" bien recu")
+                        dat += self.request.recv(1024).strip()
+                        text = dat.decode()
+                        if re.search(r";[A-Za-z]*.\.[A-Za-z]*", text):
+                            complete = True
+                    temp = text.split(";")
+                    data = temp[1]
+                    fileName = temp[2]
+                    FileManager.convertFileReceive(data, fileName)
+                    self.sendMsg("T", "Fichier "+fileName+" bien recu")
             else:
-                essai=0
-                identify=0
-                builtins.init=0
+                essai = 0
+                identify = 0
+                builtins.init = 0
                 print("client deconnecte")
 #                Cerveau.cleanHistory()
                 break
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    """Simple override"""
     pass
