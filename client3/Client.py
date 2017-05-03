f6cff7960ce22ea64d8d79394ee5855e694112d6
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 10:27:51 +0200

    pylint compliance

--- client3/Client.py
+++ client3/Client.py
@@ -1,95 +1,109 @@
+"""
+Module gérant l'ensemble des communications
+Connexion au serveur
+Encodage
+Notification
+"""
+
 import socket
 import atexit
-import os
-import getpass
+import sys
 import re
 import Securite
 
-clef="toreplace"
+CLEF = "toreplace"
 HOST, PORT = "127.0.0.1", 9998
 
 try:
-#	import gtk
-	import notify2
-	notify2.init( "Luchiana" )
+#    import gtk
+    import notify2
+    notify2.init("Luchiana")
 except:
     print("Vous n'avez pas notify2")
 
 try:
-	import readline
+    import readline
 except:
-	print("No readline on Windows")
+    print("No readline on Windows")
 
 
-histfile = ".luchiana_history"
+HISTFILE = ".luchiana_history"
 try:
-    readline.read_history_file(histfile)
+    readline.read_history_file(HISTFILE)
 except:
     pass
 try:
-	atexit.register(readline.write_history_file, histfile)
+    atexit.register(readline.write_history_file, HISTFILE)
 except:
-	pass
+    pass
 
 def encodeMsg(message):
-    texte=Securite.vigenere(message,clef,"1")
+    """Permet de chiffrer un message"""
+    texte = Securite.vigenere(message, CLEF, "1")
     return texte
 
 def decodeMsg(message):
-    texte=Securite.vigenere(message,clef,"2")
+    """Permet de déchiffrer un message"""
+    texte = Securite.vigenere(message, CLEF, "2")
     return texte
 
-def sendMsg(typ,message):
-    message=encodeMsg(message)
-    sock.send(bytes(typ+";"+message,"utf-8"))
+def sendMsg(typ, message):
+    """Permet d'envoyer un message"""
+    message = encodeMsg(message)
+    sock.send(bytes(typ+";"+message, "utf-8"))
 
-def sendFile(data,fileName):
-    sock.send(bytes("F;","utf-8")+data+bytes(";"+fileName,"utf-8"))
+def sendFile(data, file_name):
+    """Permet d'envoyer un fichier"""
+    sock.send(bytes("F;", "utf-8")+data+bytes(";"+file_name, "utf-8"))
 
 def receiveMsg():
-    rec=sock.recv(1024).strip()
-    t=rec.decode()
-    temp=t.split(";")
-    typ=temp[0]
-    if typ=="F":
-        complete=False
+    """Permet de recevoir un message"""
+    rec = sock.recv(1024).strip()
+    recd = rec.decode()
+    temp = recd.split(";")
+    typ = temp[0]
+    if typ == "F":
+        complete = False
         while not complete:
             rec += sock.recv(1024).strip()
-            t=rec.decode()
-            if re.search(";[A-Za-z]*.\.[A-Za-z]*",t):
-                complete=True
-        temp=t.split(";")
-        text=temp[1]
-        if temp[2][-1:]=="T":
-            temp[2]=temp[2][:-1]
-        text+=";"+temp[2]
+            recd = rec.decode()
+            if re.search(r";[A-Za-z]*.\.[A-Za-z]*", recd):
+                complete = True
+        temp = recd.split(";")
+        text = temp[1]
+        if temp[2][-1:] == "T":
+            temp[2] = temp[2][:-1]
+        text += ";"+temp[2]
     else:
-        text=decodeMsg(temp[1])
-    return typ,text
+        text = decodeMsg(temp[1])
+    return typ, text
 
 def notify(phrase):
-	res="ok"
-	temp=phrase.split(";")
-	title=temp[1]
-	body=temp[2]
-	type=temp[3]
-	try:
-		n = notify2.Notification(title,body)
-		n.show()
-	except:
-		res="ko"
-		return res
+    """Notifie l'utilisateur"""
+    res = "ok"
+    temp = phrase.split(";")
+    title = temp[1]
+    body = temp[2]
+    #type = temp[3]
+    try:
+        notif = notify2.Notification(title, body)
+        notif.show()
+    except:
+        res = "ko"
+        return res
 
 def clientQuit():
-	print("A bientot")
-	sock.close()
-	os._exit(0)
+    """Ferme la socket"""
+    print("A bientot")
+    sock.close()
+    sys.exit(0)
 
 def connect():
-	global sock
-	# Create a socket (SOCK_STREAM means a TCP socket)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	try:
-	    sock.connect((HOST, PORT))
-	except ValueError:
-	    print("there is an error")
+    """Ouvre la socket"""
+    global sock
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((HOST, PORT))
+    except ValueError:
+        print("there is an error")
