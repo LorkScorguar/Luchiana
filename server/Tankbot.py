4256ccb7124a592b18d82af4a69c6384683a6cc7
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2017-05-03 11:42:48 +0200

    pylint compliance

--- server/Tankbot.py
+++ server/Tankbot.py
@@ -1,33 +1,36 @@
-#!/usr/bin/python3.2
-
+#!/usr/bin/python3
+"""
+Module pour piloter un Tankbot en utilisant l'emetteur fournit
+"""
 import subprocess
 import time
 import re
 
-def play(type):
-    direction=""
-    if re.search("avance",type):
-        direction="lfrf"
-    elif re.search("recule",type):
-        direction="lbrb"
-    elif re.search("gauche",type):
-        direction="lbrf"
-    elif re.search("droite",type):
-        direction="lfrb"
-    elif re.search("fire",type):
-        direction="fire"
+def play(typ):
+    """fonction pour lire le bon ficheir son selon l'action demandée"""
+    direction = ""
+    if re.search("avance", typ):
+        direction = "lfrf"
+    elif re.search("recule", typ):
+        direction = "lbrb"
+    elif re.search("gauche", typ):
+        direction = "lbrf"
+    elif re.search("droite", typ):
+        direction = "lfrb"
+    elif re.search("fire", typ):
+        direction = "fire"
     else:
-        direction="lsrs"
-    file=direction+"orangetank"
-    file=file+"flip.wav"
+        direction = "lsrs"
+    file = direction+"orangetank"
+    file = file+"flip.wav"
     #print("Testing "+type)
-    proc=subprocess.Popen(["mplayer","database/raw/"+file],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    proc = subprocess.Popen(["mplayer", "database/raw/"+file],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout = proc.communicate()[0]
     time.sleep(1)
 
-"""play("avance")
-play("gauche")
-play("avance")
-play("recule")
-play("stop")
-"""
+#play("avance")
+#play("gauche")
+#play("avance")
+#play("recule")
+#play("stop")
